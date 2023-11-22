1ca9f223f00c90c0b223c2e5b5168413ba2c5b50
LinFight <1095722147@qq.com>
2023-11-22 12:49:59 +0800

    doc:add

--- tutorial_display_image.py
+++ tutorial_display_image.py
@@ -1,10 +1,19 @@
 import cv2 as cv
 import sys
 
+# 通过文件路径+文件名，读取图片文件
 img = cv.imread("./samples/starry_night.jpg")
+
+# 检测是否读取到图片，测试下来如果上一步没有读取到图片,直接给warning
 if img is None:
     sys.exit("Could not read the image.")
+
+# GUI 功能，winname 是窗口的名字
 cv.imshow("Display window", img)
+
+# 检测一次检测输入
 k = cv.waitKey(0)
+
+# 如果按键是s就将图片写到指定目录,ord 这个函数对键盘控制很有用
 if k == ord("s"):
     cv.imwrite("./output/starry_night.png", img)
