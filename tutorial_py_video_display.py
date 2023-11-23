16636d3208d65234e69f02cc76f13d15c8834578
LinFight <1095722147@qq.com>
2023-11-23 16:20:10 +0800

    fea:change fps

--- tutorial_py_video_display.py
+++ tutorial_py_video_display.py
@@ -15,7 +15,7 @@ cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 print("摄像头的像素是{}*{}".format(cap.get(cv.CAP_PROP_FRAME_WIDTH), cap.get(cv.CAP_PROP_FRAME_HEIGHT)))
 # 设置摄像头的帧率
-cap.set(cv.CAP_PROP_FPS, 30)
+cap.set(cv.CAP_PROP_FPS, 120)
 print("摄像头的帧率是{}".format(cap.get(cv.CAP_PROP_FPS)))
 
 # 死循环，逐帧读取摄像头信息
